518a36f9dd15537042101e2da6a9a86ccb936394
Omid Kashan <omid@antilong.com>
2024-08-08 22:34:45 +0100

    reenable react transpile step

--- packages/react/scripts/build.js
+++ packages/react/scripts/build.js
@@ -1,49 +1,63 @@
-// import { rollup } from "rollup"
-// import { nodeResolve as rollupNodeResolve } from "@rollup/plugin-node-resolve"
-// import { default as rollupTypescript } from "@rollup/plugin-typescript"
-// import path, { resolve } from "node:path"
-// import packageJson from "../package.json" with { type: "json" }
-
-// const __dirname = resolve()
-// const outDir = "lib"
-// const rootDir = "src"
-
-// async function build() {
-//   const plugins = [
-//     rollupNodeResolve(),
-//     rollupTypescript({
-//       outDir,
-//       rootDir,
-//     }),
-//   ]
-//   const external = ["react", "react-dom", "react/jsx-runtime"]
-//   const bundle = await rollup({ input: "src/index.ts", plugins, external })
-
-//   await bundle.write({
-//     dir: outDir,
-//     exports: "named",
-//     format: "esm",
-//     preserveModules: true,
-//     preserveModulesRoot: rootDir,
-//   })
-// }
-
-// build().catch((error) => {
-//   console.log(error)
-//   process.exit(1)
-// })
-
+import { rollup } from "rollup"
+import { nodeResolve as rollupNodeResolve } from "@rollup/plugin-node-resolve"
+import { default as rollupTypescript } from "@rollup/plugin-typescript"
+//import path, { resolve } from "node:path"
+//import packageJson from "../package.json" with { type: "json" }
 import fs from "fs-extra"
-;(async () => {
-  const components = await fs.readdir("./src/components")
+const rootDir = "src"
 
-  if (components.length) {
-    await fs.emptyDir("./lib")
+async function build() {
+  const formats = [
+    {
+      type: "esm",
+      outDir: "esm",
+    },
+    {
+      type: "commonjs",
+      outDir: "cjs",
+    },
+  ]
+  for (const format of formats) {
+    const { outDir, type } = format
+
+    await fs.emptyDir(`./${outDir}`)
+
+    const plugins = [
+      rollupNodeResolve(),
+      rollupTypescript({
+        outDir,
+        rootDir,
+      }),
+    ]
+    const external = ["react", "react-dom", "react/jsx-runtime"]
+    const bundle = await rollup({ input: "src/index.ts", plugins, external })
+
+    await bundle.write({
+      dir: outDir,
+      exports: "named",
+      format: type,
+      preserveModules: true,
+      preserveModulesRoot: rootDir,
+    })
   }
+}
 
-  for await (const component of components) {
-    await fs.copy(`./src/components/${component}`, `./lib/components/${component}`)
-  }
+build().catch((error) => {
+  console.log(error)
+  process.exit(1)
+})
 
-  await fs.copy("./src/index.ts", "./lib/index.ts")
-})()
+// import fs from "fs-extra"
+// ;(async () => {
+//   const components = await fs.readdir("./src/components")
+
+//   if (components.length) {
+//     await fs.emptyDir("./lib")
+//   }
+
+//   for await (const component of components) {
+//     await fs.copy(`./src/components/${component}`, `./lib/components/${component}`)
+//   }
+
+//   await fs.copy("./src/index.ts", "./lib/index.ts")
+// })()
