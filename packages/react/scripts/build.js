6606a10511f69f8c495d7714d733a9d3a2537e5c
Omid Kashan <omid@antilong.com>
2024-08-08 22:18:32 +0100

    remove react transpile step for now

--- packages/react/scripts/build.js
+++ packages/react/scripts/build.js
@@ -1,34 +1,49 @@
-import { rollup } from "rollup"
-import { nodeResolve as rollupNodeResolve } from "@rollup/plugin-node-resolve"
-import { default as rollupTypescript } from "@rollup/plugin-typescript"
-import path, { resolve } from "node:path"
-import packageJson from "../package.json" with { type: "json" }
+// import { rollup } from "rollup"
+// import { nodeResolve as rollupNodeResolve } from "@rollup/plugin-node-resolve"
+// import { default as rollupTypescript } from "@rollup/plugin-typescript"
+// import path, { resolve } from "node:path"
+// import packageJson from "../package.json" with { type: "json" }
 
-const __dirname = resolve()
-const outDir = "lib"
-const rootDir = "src"
+// const __dirname = resolve()
+// const outDir = "lib"
+// const rootDir = "src"
 
-async function build() {
-  const plugins = [
-    rollupNodeResolve(),
-    rollupTypescript({
-      outDir,
-      rootDir,
-    }),
-  ]
-  const external = ["react", "react-dom", "react/jsx-runtime"]
-  const bundle = await rollup({ input: "src/index.ts", plugins, external })
+// async function build() {
+//   const plugins = [
+//     rollupNodeResolve(),
+//     rollupTypescript({
+//       outDir,
+//       rootDir,
+//     }),
+//   ]
+//   const external = ["react", "react-dom", "react/jsx-runtime"]
+//   const bundle = await rollup({ input: "src/index.ts", plugins, external })
 
-  await bundle.write({
-    dir: outDir,
-    exports: "named",
-    format: "esm",
-    preserveModules: true,
-    preserveModulesRoot: rootDir,
-  })
-}
+//   await bundle.write({
+//     dir: outDir,
+//     exports: "named",
+//     format: "esm",
+//     preserveModules: true,
+//     preserveModulesRoot: rootDir,
+//   })
+// }
 
-build().catch((error) => {
-  console.log(error)
-  process.exit(1)
-})
+// build().catch((error) => {
+//   console.log(error)
+//   process.exit(1)
+// })
+
+import fs from "fs-extra"
+;(async () => {
+  const components = await fs.readdir("./src/components")
+
+  if (components.length) {
+    await fs.emptyDir("./lib")
+  }
+
+  for await (const component of components) {
+    await fs.copy(`./src/components/${component}`, `./lib/components/${component}`)
+  }
+
+  await fs.copy("./src/index.ts", "./lib/index.ts")
+})()
