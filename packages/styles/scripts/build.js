07ff256740532ec6559f7dd144c589aad2b4fe26
Omid Kashan <omid@antilong.com>
2024-12-05 16:59:16 +0000

    feat: output discrete css files for components

--- packages/styles/scripts/build.js
+++ packages/styles/scripts/build.js
@@ -3,26 +3,29 @@ import fs from "fs-extra"
 import path from "path"
 
 async function build() {
-  const input = "./scss/main.scss"
+  const inputs = [{ path: "./scss/main.scss", file: "main.scss" }]
   const loadPaths = [path.resolve("./node_modules")]
 
+  const files = await fs.readdir("./scss/components")
+
+  console.log(files)
   await fs.emptyDir("./css")
 
-  const output = sass.compile(input, {
-    //style: "compressed",
-    sourceMap: false,
-    loadPaths,
-  })
+  inputs.push(
+    ...files.map((file) => {
+      return { path: `./scss/components/${file}`, file: `components/${file}` }
+    })
+  )
+  for (const input of inputs) {
+    console.log(input)
+    const output = sass.compile(input.path, {
+      //style: "compressed",
+      sourceMap: false,
+      loadPaths,
+    })
 
-  // const output = await postcss([
-  //     autoprefixer({ grid: true }),
-  //     sortMediaQueries(), // eslint-disable-line @typescript-eslint/no-unsafe-call
-  // ]).process(compiledSass.css, {
-  //     from: input,
-  // })
-
-  //const sourceMapReference = `/*# sourceMappingURL=${outputFileName}.css.map */`
-  await fs.outputFile(`./css/main.css`, output.css)
+    await fs.outputFile(`./css/${input.file.replace("scss", "")}.css`, output.css)
+  }
 }
 
 build().catch((error) => {
